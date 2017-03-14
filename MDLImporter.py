38030175888d89f16b1fd662fec4f153b14c0373
Alastair Paterson <alas.paterson@gmail.com>
2017-03-14 17:46:57 +0000

    Added a material and texture parser
    Now able to parse and read in data about materials and textures from a (decently formatted) .mdl file

--- MDLImporter.py
+++ MDLImporter.py
@@ -26,7 +26,7 @@ bl_info = {
 	"name": "MDL Importer",
 	"description": "Imports Warcraft 3 models",
 	"author": "Yellow",
-	"version": (0,0,1),
+	"version": (0,0,2),
 	"blender": (2,7,8),
 	"location": "File > Import > MDL (.mdl)",
 	"category": "Import-Export"
@@ -76,22 +76,138 @@ class GeosetParser(Parser):
 				
 			for x in range(int(data[0])):
 				if label == "Faces":
-					ret[label].append([int(v) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")])
+					[ret[label].append(int(v)) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")]
 				if label == "Vertices":
-					ret[label].append([float(v) / 20.0 for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")])
+					[ret[label].append(float(v) / 20.0)  for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")]
 				if label == "Normals":
-					ret[label].append([float(v) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")])
+					[ret[label].append(float(v)) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")]
 				if label == "TVertices":
-					ret[label].append([float(v) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")])
+					[ret[label].append(float(v)) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")]
 		return ret
+
+class TextureParser(Parser):
+	def parse(self, context, count):
+		ret = {}
+		texture = 0
+		pars = 1
+		
+		while texture < count:
+			line = self.file.readline().strip()
+			pars = self.check_pars(pars, line)
+			label, *data = line.split(" ")
 			
+			if label != "Bitmap":
+				continue
+			
+			ret[texture] = {}
+			line = self.file.readline().replace(",", "").strip()
+
+			while self.check_pars(pars, line) == pars:
+				label, data = line.split(" ")
+				
+				if data == '""':
+					ret[texture][label] = ""
+				else:
+					ret[texture][label] = data.replace('"', "")
+					
+				line = self.file.readline().replace(",", "").strip()
+			
+			pars = self.check_pars(pars, line)
+			texture += 1
+		
+		return ret
+
+class MaterialParser(Parser):
+	def __init__(self, file):
+		self.layer_parser = LayerParser(file)
+		super(MaterialParser, self).__init__(file)
+
+	def parse(self, context, count):
+		ret = {}
+		material = 0
+		pars = 1
+
+		line = self.file.readline().strip()
+		pars = self.check_pars(pars, line)
+		
+		while material < count:
+			label, *data = line.split(" ")
+			
+			if label == "Material":				
+				ret[material] = {}
+				layer = 0
+				
+				line = self.file.readline().replace(",", "").strip()
+				pars = self.check_pars(pars, line)
+				
+				while pars > 2:
+					label, *data = line.replace(",", "").split(" ")
+					
+					if label == "Layer":
+						ret[material][layer] = self.layer_parser.parse(context)
+						layer += 1
+					
+					pars -= 1
+					line = self.file.readline().strip()
+					pars = self.check_pars(pars, line)
+					
+				material += 1
+			
+			line = self.file.readline().replace(",", "").strip()
+			pars = self.check_pars(pars, line)	
+					
+		return ret
+
+		
+class LayerParser(Parser):
+	def __init__(self, file):
+		self.tokens = ["FilterMode", "Unshaded", "TextureID", "Alpha", "TwoSided", "Linear"]
+		super(LayerParser, self).__init__(file)
+		
+	def parse(self, context):
+		layer = {}
+		pars = 1
+		
+		line = self.file.readline().replace(",", "").strip()
+		pars = self.check_pars(pars, line)
+		
+		while pars > 0:
+			label, *data = line.split(" ")
+
+			if label == "static":
+				label = data[0]
+				data = data[1:]
+			
+			if label in self.tokens:			
+				if label == "Alpha":
+					layer["Alpha"] = {}	
+					line = self.file.readline().replace(",", "").strip()
+					pars = self.check_pars(pars, line)
+					
+					while pars > 1:
+						label, *data = line.split(" ")
+						
+						layer["Alpha"][label] = data[0] if len(data) > 0 else None
+					
+						line = self.file.readline().replace(",", "").strip()
+						pars = self.check_pars(pars, line)	
+				else:
+					layer[label] = data[0] if len(data) > 0 else None
+			
+			line = self.file.readline().replace(",", "").strip()
+			pars = self.check_pars(pars, line)
+		
+		return layer
+		
 class MDLParser(Parser):
 	def __init__(self, file):
 		self.tokens = {
 			"Version": VersionParser(file),
 			"Model": None, # We only care about the model's name right now
 			"Geoset": GeosetParser(file),
-			#"Sequences": , "GlobalSequences", "Textures", "Materials", 
+			"Textures": TextureParser(file),
+			"Materials": MaterialParser(file)
+			#"Sequences": , "GlobalSequences", 
 			#"GeosetAnim", "Bone", "Helper", "Attachment", "PivotPoints"
 		}
 		self.mdl = {"Geosets": []}
@@ -112,6 +228,10 @@ class MDLParser(Parser):
 				self.mdl["Geosets"].append(self.tokens[label].parse(context))
 			elif label == "Version":
 				self.tokens["Version"].parse(context)
+			elif label == "Textures":
+				self.mdl["Textures"] = self.tokens[label].parse(context, int(data[0]))
+			elif label == "Materials":
+				self.mdl["Materials"] = self.tokens[label].parse(context, int(data[0]))
 				
 			line = self.file.readline()
 
@@ -140,19 +260,13 @@ class Importer(bpy.types.Operator, ImportHelper):
 				obj.location = (0.0, 0.0, 0.0)
 				bpy.context.scene.objects.link(obj)
 				
-				verts = [v for v3 in geoset["Vertices"] for v in v3]
+				mesh.vertices.add(len(geoset["Vertices"]) // 3)
+				mesh.vertices.foreach_set("co", geoset["Vertices"])
 				
-				mesh.vertices.add(len(verts) // 3)
-				mesh.vertices.foreach_set("co", verts)
+				mesh.tessfaces.add(len(geoset["Faces"]) // 3)
+				mesh.tessfaces.foreach_set("vertices", geoset["Faces"])
 				
-				faces = [f for ff in geoset["Faces"] for f in ff]
-				
-				mesh.tessfaces.add(len(faces) //3 )
-				mesh.tessfaces.foreach_set("vertices", faces)
-				
-				norms = [n for n3 in geoset["Normals"] for n in n3]
-				
-				mesh.vertices.foreach_set("normal", norms)
+				mesh.vertices.foreach_set("normal", geoset["Normals"])
 				mesh.update()
 
 		return {"FINISHED"}
