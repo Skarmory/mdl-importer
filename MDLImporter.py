0aa1e160c02049522f0e9a0e6fc328b07f4619b7
Alastair Paterson <alas.paterson@gmail.com>
2017-03-15 23:23:14 +0000

    Texture loading
    - Loads in textures and creates materials
    - Uses cycles to generate a node tree

--- MDLImporter.py
+++ MDLImporter.py
@@ -18,6 +18,7 @@
 #DEALINGS IN THE SOFTWARE.
 
 import bpy
+import os
 
 from bpy_extras.io_utils import ImportHelper
 from bpy.props import StringProperty
@@ -26,9 +27,9 @@ bl_info = {
 	"name": "MDL Importer",
 	"description": "Imports Warcraft 3 models",
 	"author": "Yellow",
-	"version": (0,0,2),
+	"version": (0,1,0),
 	"blender": (2,7,8),
-	"location": "File > Import > MDL (.mdl)",
+	"location": "File > Import > WC3 MDL (.mdl)",
 	"category": "Import-Export"
 }
 
@@ -55,7 +56,7 @@ class VersionParser(Parser):
 
 class GeosetParser(Parser):
 	def __init__(self, file):
-		self.tokens = ["Vertices", "Normals", "TVertices", "Faces"]
+		self.tokens = ["Vertices", "Normals", "TVertices", "Faces", "MaterialID"]
 		super(GeosetParser, self).__init__(file)
 
 	def parse(self, context):
@@ -70,9 +71,15 @@ class GeosetParser(Parser):
 			if label not in self.tokens:
 				continue
 			
+			if label == "MaterialID":
+				ret[label] = data[0].replace(",", "")
+				continue
+			
 			ret[label] = []
+			
 			if label == "Faces":
 				self.file.readline()
+				pars = self.check_pars(pars, line)
 				
 			for x in range(int(data[0])):
 				if label == "Faces":
@@ -82,7 +89,8 @@ class GeosetParser(Parser):
 				if label == "Normals":
 					[ret[label].append(float(v)) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")]
 				if label == "TVertices":
-					[ret[label].append(float(v)) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")]
+					ret[label].append([float(v) for v in self.file.readline().replace("{", "").replace("}", "").replace(",", "").strip().split(" ")])
+					
 		return ret
 
 class TextureParser(Parser):
@@ -134,25 +142,26 @@ class MaterialParser(Parser):
 			label, *data = line.split(" ")
 			
 			if label == "Material":				
-				ret[material] = {}
+				ret[material] = {"Layers": {}}
 				layer = 0
 				
 				line = self.file.readline().replace(",", "").strip()
 				pars = self.check_pars(pars, line)
 				
-				while pars > 2:
+				while pars > 1:
 					label, *data = line.replace(",", "").split(" ")
 					
 					if label == "Layer":
-						ret[material][layer] = self.layer_parser.parse(context)
+						ret[material]["Layers"][layer] = self.layer_parser.parse(context)
 						layer += 1
-					
-					pars -= 1
+						pars -= 1
+					elif label == "ConstantColor":
+						ret[material]["ConstantColor"] = True
+						
 					line = self.file.readline().strip()
 					pars = self.check_pars(pars, line)
-					
-				material += 1
-			
+				
+			material += 1
 			line = self.file.readline().replace(",", "").strip()
 			pars = self.check_pars(pars, line)	
 					
@@ -187,12 +196,12 @@ class LayerParser(Parser):
 					while pars > 1:
 						label, *data = line.split(" ")
 						
-						layer["Alpha"][label] = data[0] if len(data) > 0 else None
+						layer["Alpha"][label] = data[0] if len(data) > 0 else True
 					
 						line = self.file.readline().replace(",", "").strip()
 						pars = self.check_pars(pars, line)	
 				else:
-					layer[label] = data[0] if len(data) > 0 else None
+					layer[label] = data[0] if len(data) > 0 else True
 			
 			line = self.file.readline().replace(",", "").strip()
 			pars = self.check_pars(pars, line)
@@ -251,7 +260,77 @@ class Importer(bpy.types.Operator, ImportHelper):
 	
 		with open(self.filepath, "r") as file:
 			mdl = MDLParser(file).parse(context)
-		
+			
+			textures = []
+			materials = []
+			objs = []
+			
+			bpy.context.scene.render.engine = "CYCLES"
+			
+			# Load in the textures
+			for x in range(len(mdl["Textures"])):
+				texture = mdl["Textures"][x]
+				tex = None
+				
+				# There are some textures with no file path, these appear to be just solid colours instead
+				if not texture["Image"]:
+					if "ReplaceableId" not in texture:
+						tex = "TRANSPARENT"
+						
+					elif texture["ReplaceableId"] == "1":
+						tex = "PLAYER_COLOUR"
+						
+					elif texture["ReplaceableId"] == "2":
+						tex = "HERO_GLOW"
+						
+				else:
+					path = os.path.expanduser("~/Desktop/Work/" + texture["Image"].replace("blp", "png"))
+					tex = bpy.data.images.load(path)
+				
+				textures.append(tex)
+			
+			# Create the materials
+			for x in range(len(mdl["Materials"])):
+				material = mdl["Materials"][x]
+				mat = bpy.data.materials.new("TexMat")
+				mat.use_nodes = True
+				mat.game_settings.alpha_blend = "CLIP"
+				
+				nodes = mat.node_tree.nodes
+				links = mat.node_tree.links
+				
+				tex_image = nodes.new("ShaderNodeTexImage")
+				output = nodes["Material Output"]
+				diffuse = nodes["Diffuse BSDF"]
+				mix = nodes.new("ShaderNodeMixShader")
+				blend_colour = None
+				
+				# This bit sorts out composing layers. The layer with team colour needs to have the colour mixed in
+				# The layer without the team colours need to have transparency mixed in
+				# The colour is just set to green by default right now, change it in the "default_value" RGBA tuples
+				for layer_key in material["Layers"]:
+					layer = material["Layers"][layer_key]
+					tex = textures[int(layer["TextureID"])]
+					
+					if tex == "PLAYER_COLOUR" or tex == "HERO_GLOW":
+						blend_colour = nodes.new("ShaderNodeBsdfDiffuse")
+						blend_colour.inputs[0].default_value = (0.0, 1.0, 0.0, 1.0)
+						continue
+					
+					tex_image.image = tex
+					if not blend_colour:
+						blend_colour = nodes.new("ShaderNodeBsdfTransparent")
+						blend_colour.inputs[0].default_value = (0.0, 0.0, 0.0, 1.0)
+				
+				links.new(tex_image.outputs[0], diffuse.inputs[0])
+				links.new(tex_image.outputs[1], mix.inputs[0])
+				links.new(blend_colour.outputs[0], mix.inputs[1])
+				links.new(diffuse.outputs[0], mix.inputs[2])
+				links.new(mix.outputs[0], output.inputs[0])
+					
+				materials.append(mat)
+			
+			# Load in the meshes, and UVs, and add the materials to the correct one
 			for x in range(len(mdl["Geosets"])):
 				geoset = mdl["Geosets"][x]
 			
@@ -267,7 +346,25 @@ class Importer(bpy.types.Operator, ImportHelper):
 				mesh.tessfaces.foreach_set("vertices", geoset["Faces"])
 				
 				mesh.vertices.foreach_set("normal", geoset["Normals"])
+				
 				mesh.update()
+				
+				vi_uv = {i: (u, 1.0 - v) for i, (u, v) in enumerate(geoset["TVertices"])}
+				per_loop_list = [0.0] * len(mesh.loops)
+				for loop in mesh.loops:
+					per_loop_list[loop.index] = vi_uv[loop.vertex_index]
+				
+				per_loop_list = [uv for pair in per_loop_list for uv in pair]
+				
+				mesh.uv_textures.new("UV")
+				mesh.uv_layers[0].data.foreach_set("uv", per_loop_list)
+				
+				mat_id = int(geoset["MaterialID"])
+				mesh.materials.append(materials[mat_id])
+				
+				mesh.update()
+				
+				objs.append(obj)
 
 		return {"FINISHED"}
 
